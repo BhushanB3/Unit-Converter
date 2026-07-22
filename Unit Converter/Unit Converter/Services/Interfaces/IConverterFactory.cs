c75912873ecc4a342b35116753c9ac858a0a6f67
Bhushan <baviskar615@gmail.com>
2026-07-22 12:31:49 +0530

    created I/f for factory design Pattern

---
+++ Unit Converter/Unit Converter/Services/Interfaces/IConverterFactory.cs
@@ -0,0 +1,9 @@
+﻿using Unit_Converter.Services.IServices;
+
+namespace Unit_Converter.Services.Interfaces
+{
+    public interface IConverterFactory
+    {
+        public IUnitToConvert CreateConverter(string converterType);
+    }
+}
