fc59e819c57234bcf152b57db7df8b60917ba603
Bhushan <baviskar615@gmail.com>
2026-07-22 12:56:18 +0530

    Create Controller

---
+++ Unit Converter/Unit Converter/Controllers/UnitConverterController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Unit_Converter.Enums;
+using Unit_Converter.Services.Interfaces;
+
+namespace Unit_Converter.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UnitConverterController : ControllerBase
+    {
+
+        private readonly IConverterFactory _converterFactory;
+
+        public UnitConverterController(IConverterFactory converterFactory)
+        {
+            _converterFactory = converterFactory;
+        }
+
+        [HttpGet]
+        [Route("Convert")]
+        public IActionResult ConvertUnit([FromQuery] string unitType, [FromQuery] ConverterUnits value)
+        {
+            return Ok(null);
+        }
+    }
+}
