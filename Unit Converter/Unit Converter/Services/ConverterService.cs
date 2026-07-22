1a93690ca9170ab675db0e94b5d7fec236b4d8ca
Bhushan <baviskar615@gmail.com>
2026-07-22 17:17:24 +0530

    Added service File

---
+++ Unit Converter/Unit Converter/Services/ConverterService.cs
@@ -0,0 +1,30 @@
+﻿using Unit_Converter.Enums;
+using Unit_Converter.Services.Interfaces;
+
+namespace Unit_Converter.Services
+{
+    public class ConverterService : IConverterService
+    {
+        private readonly IConverterFactory _converterFactory;
+
+        public ConverterService(IConverterFactory converterFactory)
+        {
+            _converterFactory= converterFactory;
+        }
+
+        public string Convertunits(ConverterUnits unitType, double value)
+        {
+            var converter = _converterFactory.CreateConverter(unitType);
+            if(converter != null)
+            {
+                var convertedValue = converter.Convert(value);
+                return convertedValue;
+            }
+            else
+            {
+                return null;
+            }
+                
+        }
+    }
+}
---
+++ Unit Converter/Unit Converter/Services/Interfaces/IConverterService.cs
@@ -0,0 +1,9 @@
+﻿using Unit_Converter.Enums;
+
+namespace Unit_Converter.Services.Interfaces
+{
+    public interface IConverterService
+    {
+        public string Convertunits(ConverterUnits converterType, double value);
+    }
+}
