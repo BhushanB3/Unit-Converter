780750a26b3dcc9d1e3bfc31da068521ba9ecedf
Bhushan <baviskar615@gmail.com>
2026-07-22 17:24:15 +0530

    Added Conversion Constants

---
+++ Unit Converter/Unit Converter/Constants/ConversionConstants.cs
@@ -0,0 +1,12 @@
+﻿namespace Unit_Converter.Constants
+{
+    public static class ConversionConstants
+    {
+        public const double MetersToFeet = 3.28084;
+
+        public const double CelciusToFahrenitMultipler = 1.8;
+        public const double CelciusToFahrenitAddition = 32;
+
+        public const double KilogramToPound = 2.20462
+    }
+}
